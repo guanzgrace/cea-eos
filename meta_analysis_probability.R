49b605e59f74d19184f009e293be83ce19257b2b
Grace Guan <gg17@alumni.princeton.edu>
2021-03-10 20:31:05 -0800

    clean directory and add meta analysis in R

---
+++ meta_analysis_probability.R
@@ -0,0 +1,40 @@
+library(readr)
+library(meta)
+library(tibble)
+library(Hmisc)
+
+ptf <- read_csv("Dropbox/0_Stanford/Winter2020/HRP 263/Notebooks/Data/prob_random_effects_input.csv")
+
+ptf <- as_tibble(cbind(ptf, binconf(ptf$X, ptf$N)))
+
+results <- data.frame(variable=character(), mean=double(),
+                      lower=double(), upper=double())
+
+for (var in unique(ptf$Variable)) {
+  madata <- ptf[ptf$Variable == var, ]
+  
+  m.hksj <- metagen(PointEst,
+                    data = madata,
+                    studlab = paste(Study),
+                    comb.fixed = FALSE,
+                    comb.random = TRUE,
+                    #method.tau = "SJ",
+                    #hakn = TRUE,
+                    prediction = TRUE,
+                    lower=Lower,
+                    upper=Upper)#,
+                    #sm = "SMD")
+  print(m.hksj)
+  
+  de <- list(variable=var, mean=m.hksj$TE.random,
+             lower=m.hksj$lower.random, upper=m.hksj$upper.random)
+  results = rbind(results, de)
+}
+
+results$lower <- ifelse(results$lower < 0, 10^-6, results$lower)
+print(results)
+
+write.csv(results, "Dropbox/0_Stanford/Winter2020/HRP 263/Notebooks/Data/prob_random_effects_output.csv")
+
+
+
